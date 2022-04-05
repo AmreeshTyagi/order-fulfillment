bcc086be47f2642a2387abe43ff2d8ce4ed9805a
AmreeshTyagi <tyagi.amreesh@gmail.com>
2022-04-05 21:32:59 +0530

    adding interfaces & enums

---
+++ src/interface/index.ts
@@ -0,0 +1,45 @@
+import { ORDER_STATUS } from "../helper/enum";
+
+export interface IOrderProcessor {
+  kitchen: IKitchen;
+  dispatcher: ICourierDispatcher;
+
+  receiveOrder(order: IOrder): void;
+  prepareOrder(order: IOrder): void;
+  dispatchCourier(order: IOrder): void;
+}
+
+export interface IKitchen {
+  preparedQueue: IOrder[];
+
+  prepareOrder(order: IOrder): Promise<ORDER_STATUS>;
+}
+
+export interface IOrder {
+  id: String;
+  name: String;
+  prepTime: Number;
+}
+
+export interface ICourierDispatcher {
+  arrivedQueue: [ICourier];
+  dispatchOrderQueue: [ICourier];
+  courierWaitQueue: [ICourier];
+  strategy: IDispatchStrategy;
+  courierDelay: Number;
+
+  dispatchCourier(order: IOrder);
+}
+
+export interface ICourier {
+  courierId: String;
+  orderId: String;
+  arrivedTs: Number;
+}
+
+export interface IDispatchStrategy {
+  setOrder(order: IOrder): void;
+  getOrder(order: IOrder): void;
+  setCourier(courier: ICourier): void;
+  getCourier(courier: ICourier): void;
+}
